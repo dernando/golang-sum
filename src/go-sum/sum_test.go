48df2fe17c45616e294cc547fbff1cb583220d92
Dernando Nascimento <dernandonc@gmail.com>
2020-11-02 15:55:20 -0300

    adding unit test first

--- src/go-sum/sum_test.go
+++ src/go-sum/sum_test.go
@@ -7,7 +7,7 @@ func TestSum(t *testing.T) {
 	resultSum := sumInt(5,5)
 
 	if resultSum != 10 {
-		t.Errorf("Resultado esperado=10, resultado recebido %v", resultSum)
+		t.Errorf("Resultado esperado=10, resultado recebido %d", resultSum)
 	}
 
 }
