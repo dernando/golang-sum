e8d3bf5e827f244b5fe13d7ac1b71ac2500a8dcd
Dernando Nascimento <dernandonc@gmail.com>
2020-11-02 12:20:44 -0300

    src dir

---
+++ src/main.go
@@ -0,0 +1,15 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+	var a, b = 5, 5
+	sumInt(a, b)
+}
+
+func sumInt(a int, b int){
+	var result = a+b
+	fmt.Println(result)
+}
