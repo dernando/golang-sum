007fb0768825eab94aee4c1fb854cdec849799ac
Dernando Nascimento <dernandonc@gmail.com>
2020-11-02 14:45:53 -0300

    fix resultSum variable

--- sum_test.go
+++ sum_test.go
@@ -7,7 +7,7 @@ func TestSum(t *testing.T) {
 	resultSum := sumInt(5,5)
 
 	if result != 10 {
-		t.Errorf("Resultado esperado=10, resultado recebido %v", result)
+		t.Errorf("Resultado esperado=10, resultado recebido %v", resultSum)
 	}
 
 }
