6d66be31ddc3f5d62ec095e61ea1ad87dc1ae2f1
Dernando Nascimento <dernandonc@gmail.com>
2020-11-02 16:16:05 -0300

    change := to =

--- src/go-sum/sum_test.go
+++ src/go-sum/sum_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestSum(t *testing.T) {
 
 	var a, b = 5, 5
-	var resultSum := sumInt(5,5)
+	var resultSum = sumInt(5,5)
 
 	if resultSum != 10 {
 		t.Errorf("Resultado esperado=10, resultado recebido %d", resultSum)
