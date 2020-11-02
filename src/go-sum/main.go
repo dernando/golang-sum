bb607dd729b0cdb898c04bc82a23cc66f0234b66
Dernando Nascimento <dernandonc@gmail.com>
2020-11-02 14:52:30 -0300

    go-sum dir

---
+++ src/go-sum/main.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	var a, b = 5, 5
+	sumInt(a, b)
+}
---
+++ src/go-sum/sum.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func sumInt(a int, b int){
+	var result = a+b
+	fmt.Println(result)
+}
